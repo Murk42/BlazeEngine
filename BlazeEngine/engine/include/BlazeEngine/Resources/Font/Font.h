a42a1bfe4816e57d04d93e9eb01eadf8b1898119
Marko Radojevic <marko.d.radojevic@gmail.com>
2022-07-05 16:51:51 +0200

    .

--- BlazeEngine/engine/include/BlazeEngine/Resources/Font/Font.h
+++ BlazeEngine/engine/include/BlazeEngine/Resources/Font/Font.h
@@ -5,56 +5,76 @@
 #include "BlazeEngine/DataStructures/StringViewUTF8.h"
 #include "BlazeEngine/DataStructures/Vector.h"
 #include "BlazeEngine/DataStructures/Buffer.h"
+#include "BlazeEngine/DataStructures/Rect.h"
 #include "BlazeEngine/Graphics/GraphicsLibrary.h"
 #include <vector>
 #include <unordered_map>
 
 namespace Blaze
 {		
-	enum class FontType
+	class Font;
+
+	struct CharacterData
 	{
-		Monochrome,
-		Antialiased,		
+		float advance;
+		Vec2f renderOffset;
+		Vec2f size;
+
+		Vec2f uv1;
+		Vec2f uv2;
+	};
+
+	struct CharacterMetrics
+	{		
+		float advance;
+		Vec2f renderOffset;
+		Vec2f size;
+	};
+		               
+	class BLAZE_API FontResolution
+	{		
+		struct CharacterUV
+		{
+			Vec2f uv1;
+			Vec2f uv2;
+
+			CharacterMetrics* metrics;
+		};
+
+		Font* font;
+		uint resolution;
+		Graphics::Core::Texture2D atlas;
+		std::unordered_map<uint, CharacterUV> characterMap;
+		
+		void LoadAtlas();
+	public:
+
+		CharacterData GetCharacterData(UnicodeChar ch);
+		uint GetResolution() const { return resolution; }		
+
+		friend class Font;
 	};
 
 	class BLAZE_API Font
-	{
-	public:
-		struct CharacterData
-		{
-			int character;
-			Vec2f uv1;
-			Vec2f uv2;
-			Vec2i size;
-			Vec2i offset;
-			uint advance;
-		};
+	{		
+		void* ptr;					
 
-		struct CharacterVertex
-		{
-			Vec2f p1;
-			Vec2f p2;
-			Vec2f uv1;
-			Vec2f uv2;					
-		};
-	private:
-		void* ptr;						
-		std::unordered_map<uint, CharacterData> characterMap;
-		Graphics::Core::Texture2D texture;
-		FontType type;
-		uint resolution;
+		std::list<FontResolution> resolutions;				
+		std::unordered_map<uint, CharacterMetrics> characterMap;		
 		float baselineDistance;
+		uint characterCount;
+		uint pixelsPerUnit; //How many pixels fit in one font unit
+
+		void LoadFontMetrics();
 	public:
 		Font();		
-		~Font();		
-
-		bool GetCharacterData(UnicodeChar character, CharacterData& data) const;
-		CharacterVertex* GenerateVertices(StringViewUTF8 text, uint& vertexCount, Vec2f& size, Vec2f& bottomLeft, Vec2f& topRight) const;
-		
-		int Load(StringView path, FontType type, uint resolution, bool emitLogOnFail = true);
-
-		inline Graphics::Core::Texture2D& GetTexture() { return texture; }
-		inline FontType GetType() const { return type; }
+		~Font();				
+					
+		int Load(StringView path, bool emitLogOnFail = true);
+		FontResolution* CreateFontResolution(uint resolution);				
+				
 		inline uint GetBaselineDistance() const { return baselineDistance; }
+
+		friend class FontResolution;
 	};	
 }
